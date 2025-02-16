b459952252176571d30c0680da58b1fd67201ff5
Hillary Robert <roberthillary30@gmail.com>
2025-02-16 11:41:08 +0200

    Working with jsx components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,20 @@
 import WelcomeMessage from './components/WelcomeMessage'
 
+import Header from './components/Header'
+
+import Footer from './components/Footer'
+
+import MainContent from './components/MainContent'
+
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
+
+      <Header />
+      <MainContent />
+      <Footer />
 
       <WelcomeMessage />
 
