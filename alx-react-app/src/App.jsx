6eda140ddaf8123c56b6c13055f7484706d070e7
Hillary Robert <roberthillary30@gmail.com>
2025-02-16 11:48:23 +0200

    Working with react props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,14 +6,22 @@ import Footer from './components/Footer'
 
 import MainContent from './components/MainContent'
 
+import UserProfile from './components/UserProfile'
+
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
 
+
+      
       <Header />
+
       <MainContent />
+
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      
       <Footer />
 
       <WelcomeMessage />
