2c365e2b9eaf8ad947a07344fae71b25f3b00684
Hillary Robert <roberthillary30@gmail.com>
2025-02-23 16:55:46 +0200

    react router

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <>
+
+    <nav style={{padding: "1rem", backgroundColor: "#f0f0f0"}}>
+
+      <ul style={{display: "flex", flexDirection:"row", listStyle: "none", gap: "1rem", textDecoration: "none"}}>
+
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li style={{}}>
+
+          <Link to="/about">About</Link>
+
+        </li>
+
+
+        <li style={{}}>
+
+          <Link to="/services">Services</Link>
+
+        </li>
+
+
+        <li style={{}}>
+
+      
+          <Link to="/contact">Contact</Link>
+
+        </li>
+
+
+
+
+
+      </ul>
+
+
+
+    </nav>
+
+
+    </>
+  )
+}
+
+export default Navbar
