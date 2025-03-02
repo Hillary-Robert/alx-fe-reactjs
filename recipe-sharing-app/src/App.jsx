d4fa927f66274f914ed8d28fdc9276dfa4c6a1f0
Hillary Robert <roberthillary30@gmail.com>
2025-03-02 10:54:36 +0200

    zustand

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import AddRecipeForm from './Components/AddRecipeForm'
+import RecipeList from './Components/RecipeList'
+
+
+function App() {
+  
+
+  return (
+    <>
+
+      <RecipeList></RecipeList>
+      <AddRecipeForm></AddRecipeForm>
+
+      
+    </>
+  )
+}
+
+export default App
