d1e61b18b652de7a8706fca89177e23403f8fbdc
Hillary Robert <roberthillary30@gmail.com>
2025-03-09 12:20:30 +0200

    react router advance

---
+++ react-router-advanced/src/components/ProfileSettings.jsx
@@ -0,0 +1,9 @@
+function ProfileSettings() {
+    return (
+        <div>
+            <h2>Profile Settings</h2>
+            <p>Here you can update your profile settings.</p>
+        </div>
+    );
+}
+export default ProfileSettings;
